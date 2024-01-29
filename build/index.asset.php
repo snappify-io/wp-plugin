3fa68cb1ab467cbd5c36a68343a16b2db0659830
Martin Staudt <ms@aptex.de>
2024-01-29 14:32:00 +0100

    run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '59244ee65b7aa4f536ca');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '8ed093dc0923575d5522');
